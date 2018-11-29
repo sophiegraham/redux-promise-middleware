79b8f77fe9882dcceac0cf0b881fd1690bf1f3ef
Sophie Graham <sophiergraham1@gmail.com>
2018-11-29 10:33:32 -0800

    started index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+const isPromise = payload => true
+export default ({ dispatch }) => next => action => {
+  if (action.payload || !isPromise(payload) return next(action);
+  store.dispatch({ type: LOAD_START });
+  action.payload
+    .then err => {
 
+    }
+    .catch err=> {
 
-
+    }
+};
